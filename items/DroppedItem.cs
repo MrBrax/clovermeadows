35959a6555523c4a76f492b35d5d87b31c7a1a77
CATERPILLAR\Braxen <braxen@braxnet.org>
2024-05-08 14:45:16 +0200

    Create DroppedItem.cs

---
+++ items/DroppedItem.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace vcrossing;
+
+public class DroppedItem : Node3D
+{
+	[Export] public string ItemDataPath;
+
+	public ItemData ItemData => ResourceLoader.Load<ItemData>( ItemDataPath );
+	
+}
