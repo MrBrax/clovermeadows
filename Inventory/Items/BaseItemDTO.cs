e2c641f0d664fd1d4d4d3191d22491224e6e4005
CATERPILLAR\Braxen <braxen@braxnet.org>
2024-05-08 11:07:07 +0200

    e

---
+++ Inventory/Items/BaseItemDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace vcrossing.Inventory.Items;
+
+public class BaseItemDTO
+{
+	
+	public virtual string GetName()
+	{
+		return "Base Item";
+	}
+	
+	public virtual string GetDescription()
+	{
+		return "This is a base item.";
+	}
+
+	public virtual ItemData ItemData { get; set; }
+
+}
