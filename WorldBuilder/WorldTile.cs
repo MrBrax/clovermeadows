884b3316451ba3a9ed1752e2c240ddbe1d68afae
Braxen <braxen@braxnet.org>
2024-05-09 10:57:16 +0200

    e

---
+++ WorldBuilder/WorldTile.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+using Godot.Collections;
+
+namespace vcrossing.WorldBuilder;
+
+public partial class WorldTile : Node3D
+{
+
+	// 4x4 grid for blocking placement
+	[Export] public bool[,] Grid = new bool[4, 4];
+
+}
