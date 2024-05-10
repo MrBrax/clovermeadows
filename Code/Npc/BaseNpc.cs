bbade5c6041a5e348799ad32edc51a56586aece3
Braxen <braxen@braxnet.org>
2024-05-11 00:20:11 +0200

    npc walk

--- Code/Npc/BaseNpc.cs
+++ Code/Npc/BaseNpc.cs
@@ -9,8 +9,10 @@ public partial class BaseNpc : CharacterBody3D
 	[Export] public Node3D Model { get; set; }
 	[Export] public NavigationAgent3D NavigationAgent { get; set; }
 	// public virtual string Description { get; set; }
-	
+
 	[Export] public float WalkSpeed { get; set; } = 2.0f;
+	[Export] public float RotationSpeed { get; set; } = 2.0f;
+	[Export] public float Acceleration { get; set; } = 2f;
 	private Vector3 TargetPosition { get; set; }
 
 	public Vector3 MovementTarget
@@ -19,70 +21,153 @@ public partial class BaseNpc : CharacterBody3D
 		set => NavigationAgent.TargetPosition = value;
 	}
 
-	public enum CurrentState { Idle, Walking, Talking, Interacting, Pose }
+	public enum CurrentState
+	{
+		Idle,
+		Waiting,
+		Walking,
+		Talking,
+		Interacting,
+		Pose
+	}
 
 	public CurrentState State { get; set; }
+
+	private float WaitingTime { get; set; }
+
 
 	public override void _Ready()
 	{
 		base._Ready();
 		SetState( CurrentState.Idle );
-		
+
 		// NavigationAgent = GetNode<NavigationAgent3D>( "NavigationAgent" );
 		NavigationAgent.PathDesiredDistance = 0.5f;
 		NavigationAgent.TargetDesiredDistance = 0.5f;
-		
+
+		SelectRandomActivity();
 		// Callable.From( ActorSetup ).CallDeferred();
 	}
-	
+
 	public void SetTargetPosition( Vector3 position )
 	{
+		GD.Print( $"Setting target position to {position}" );
 		MovementTarget = position;
+		SetState( CurrentState.Walking );
 	}
 
 	public void SetState( CurrentState state )
 	{
 		State = state;
 	}
-	
+
 	public void LookAtNode( Node3D node )
 	{
 		var target = node.GlobalTransform.Origin;
 		var position = GlobalTransform.Origin;
 		var direction = target - position;
 		Model.GlobalTransform = Model.GlobalTransform.LookingAt( position - direction, Vector3.Up );
-		
 	}
 
 	public virtual void OnInteract( PlayerInteract player )
 	{
 		GD.Print( $"Interacting with {Name}" );
+		WishVelocity = Vector3.Zero;
+		SetState( CurrentState.Interacting );
 		LookAtNode( player );
-
-		SetTargetPosition( new Vector3( 4, 0, 4 ) );
+		// SetTargetPosition( new Vector3( 4, 0, 4 ) );
 	}
+	
+	private Vector3 WishVelocity { get; set; }
 
 	public override void _PhysicsProcess( double delta )
 	{
 		base._PhysicsProcess( delta );
 
+		/*if ( State == CurrentState.Idle )
+		{
+			GoToRandomPosition();
+		}*/
+		
+		Velocity = Velocity.Lerp( WishVelocity, (float)delta * Acceleration );
+		MoveAndSlide();
+
+		if ( State == CurrentState.Waiting )
+		{
+			WaitingTime -= (float)delta;
+			if ( WaitingTime <= 0 )
+			{
+				GD.Print( "Waiting time is over" );
+				SelectRandomActivity();
+			}
+
+			return;
+		}
+
+		if ( State == CurrentState.Walking )
+		{
+			WalkToTarget( delta );
+		}
+	}
+
+	private void SelectRandomActivity()
+	{
+		var random = GD.Randf();
+		if ( random < 0.5f )
+		{
+			GD.Print( "Going to random position" );
+			GoToRandomPosition();
+		}
+		else
+		{
+			GD.Print( "Waiting" );
+			WaitingTime = GD.RandRange( 1, 5 );
+			SetState( CurrentState.Waiting );
+		}
+	}
+
+	private void WalkToTarget( double delta )
+	{
 		if ( NavigationAgent.IsNavigationFinished() )
 		{
+			GD.Print( "Reached target position" );
+			
+			// Velocity = Velocity.Lerp( Vector3.Zero, (float)(delta * 0.1f) );
+			// MoveAndSlide();
+			WishVelocity = Vector3.Zero;
+			
+			SelectRandomActivity();
 			return;
 		}
 
 		var currentAgentPosition = GlobalTransform.Origin;
 		var nextPathPosition = NavigationAgent.GetNextPathPosition();
-		
+
 		// Velocity = ( nextPathPosition - currentAgentPosition ).Normalized() * WalkSpeed;
-		Velocity = currentAgentPosition.DirectionTo( nextPathPosition ) * WalkSpeed;
-		MoveAndSlide();
+		WishVelocity = currentAgentPosition.DirectionTo( nextPathPosition ) * WalkSpeed;
+
+		var direction = nextPathPosition - currentAgentPosition;
+		// Model.GlobalTransform = Model.GlobalTransform.LookingAt( currentAgentPosition - direction, Vector3.Up );
+		// smoothly rotate the player model towards the direction
+		var targetRotation = Mathf.Atan2( direction.X, direction.Z );
+		var currentRotation = Model.Rotation.Y;
+		var newRotation = Mathf.LerpAngle( currentRotation, targetRotation, (float)delta * RotationSpeed );
+		newRotation = Mathf.Wrap( newRotation, -Mathf.Pi, Mathf.Pi );
+		Model.Rotation = new Vector3( 0, newRotation, 0 );
+
+		// MoveAndSlide();
 	}
-	
+
+	public void GoToRandomPosition()
+	{
+		var randomPosition = new Vector3( GD.Randf() * 10, 0, GD.Randf() * 10 ) + new Vector3( 4, 0, 4 );
+		SetTargetPosition( randomPosition );
+	}
+
 	/*private async void ActorSetup()
 	{
 		await ToSignal( GetTree(), SceneTree.SignalName.PhysicsFrame );
-		
-		
+
+
 	}*/
 }
--- Code/Player/PlayerCamera.cs
+++ Code/Player/PlayerCamera.cs
@@ -5,19 +5,23 @@ using vcrossing2.Code.Player;
 
 public partial class PlayerCamera : Camera3D
 {
-	
 	private PlayerController Player => GetNode<PlayerController>( "../" );
 	private World World => GetNode<WorldManager>( "/root/Main/WorldContainer" ).ActiveWorld;
-	
+
 	private Vector2I CurrentAcre = new Vector2I( 0, 0 );
-	
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _Process( double delta )
+	{
+		
+	}
+
+	private void AcreCamera( double delta )
 	{
 		var newAcreCheck = World.GetAcreFromWorldPosition( Player.GlobalPosition );
 		// GD.Print( $"Player is in acre {newAcreCheck}" );
@@ -33,21 +37,21 @@ public partial class PlayerCamera : Camera3D
 
 		// Vector3 minCameraPos = Vector3.Zero;
 		// Vector3 maxCameraPos = new Vector3( acreWidth, 0, acreHeight );
-		var minCameraPos = new Vector3( acreWidth * CurrentAcre.X + edgeBoundsX, 0, acreHeight * CurrentAcre.Y + edgeBoundsY );
-		var maxCameraPos = new Vector3( acreWidth * CurrentAcre.X + acreWidth - edgeBoundsX, 0, acreHeight * CurrentAcre.Y + acreHeight - edgeBoundsY );
-		
+		var minCameraPos = new Vector3( acreWidth * CurrentAcre.X + edgeBoundsX, 0,
+			acreHeight * CurrentAcre.Y + edgeBoundsY );
+		var maxCameraPos = new Vector3( acreWidth * CurrentAcre.X + acreWidth - edgeBoundsX, 0,
+			acreHeight * CurrentAcre.Y + acreHeight - edgeBoundsY );
+
 		var playerPos = Player.GlobalPosition;
-		
+
 		var cameraPosX = Mathf.Clamp( playerPos.X, minCameraPos.X, maxCameraPos.X );
 		var cameraPosZ = Mathf.Clamp( playerPos.Z, minCameraPos.Z, maxCameraPos.Z );
-		
+
 		var cameraPos = new Vector3( cameraPosX, 0, cameraPosZ );
-		
+
 		cameraPos += new Vector3( 0, 8f, 5f );
-		
+
 		GlobalPosition = GlobalPosition.Lerp( cameraPos, (float)delta * 5f );
-		
-		
 	}
 
 	private void AcreChanged( Vector2I newAcreCheck )
