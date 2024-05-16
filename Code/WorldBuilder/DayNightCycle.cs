c7a48f825ac13309c479610bd5438e5dfd91d23d
Braxen <braxen@braxnet.org>
2024-05-16 15:49:41 +0200

    Update DayNightCycle.cs

--- Code/WorldBuilder/DayNightCycle.cs
+++ Code/WorldBuilder/DayNightCycle.cs
@@ -22,28 +22,8 @@ public partial class DayNightCycle : Node3D
 
 	public override void _Process( double delta )
 	{
-		var time = TimeOfDay;
-		var hours = time / 3600;
-		var minutes = (time % 3600) / 60;
-		var seconds = time % 60;
-
-		/*var angle = (float)((hours % 24) / 24 * Mathf.Pi * 2) * Speed;
-		angle = angle % 360;
-		Logger.Info( "DayNightCycle", $"Time: {hours:00}:{minutes:00}:{seconds:00}, Angle: {angle}" );*/
-		float sunAngleDegrees = 0;
-		if ( hours >= 0 && hours < 6 )
-		{
-			sunAngleDegrees = Mathf.Lerp( 180, 90, (float)hours / 6 );
-		}
-		else if ( hours >= 6 && hours < 18 )
-		{
-			sunAngleDegrees = Mathf.Lerp( 90, -90, (float)(hours - 6) / 12 );
-		}
-		else if ( hours >= 18 && hours < 24 )
-		{
-			sunAngleDegrees = Mathf.Lerp( -90, -180, (float)(hours - 18) / 6 );
-		}
-		Sun.RotationDegrees = new Vector3( Mathf.Clamp( sunAngleDegrees, -MaxSunAngle, MaxSunAngle ), 45, 0 );
+		var sunAngleDegrees = (Time.GetTicksMsec() * Speed) % 360;
+		// Sun.RotationDegrees = new Vector3( Mathf.Clamp( sunAngleDegrees, -MaxSunAngle, MaxSunAngle ), 45, 0 );
 		
 		if ( Math.Abs( Sun.RotationDegrees.X ) >= MaxSunAngle )
 		{
