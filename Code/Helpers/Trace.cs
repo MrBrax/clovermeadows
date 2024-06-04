b0e622675ad55998e2c5a9dcc4eed40bd674021a
Braxen <braxen@braxnet.org>
2024-06-04 16:51:58 +0200

    shape trace

--- Code/Helpers/Trace.cs
+++ Code/Helpers/Trace.cs
@@ -10,7 +10,8 @@ public class Trace
 	{
 		_space = space;
 	}
-	
+
+
 	public class TraceResult
 	{
 		public Vector3 Position;
@@ -21,7 +22,20 @@ public class Trace
 		public object Shape;
 		public object Rid;
 	}
-	
+
+	public class ShapeTraceResult
+	{
+		public int ColliderId;
+		public object Collider;
+		public object Shape;
+		public object Rid;
+
+		public void DebugPrint()
+		{
+			GD.Print( $"ColliderId: {ColliderId}, Collider: {Collider}, Shape: {Shape}, Rid: {Rid}" );
+		}
+	}
+
 	public TraceResult CastRay( PhysicsRayQueryParameters3D parameters )
 	{
 		var result = _space.IntersectRay( parameters );
@@ -29,7 +43,7 @@ public class Trace
 		{
 			return null;
 		}
-		
+
 		return new TraceResult
 		{
 			Position = (Vector3)result["position"],
@@ -40,7 +54,30 @@ public class Trace
 			Shape = result["shape"],
 			Rid = result["rid"]
 		};
+
 	}
-		
-	
+
+	public IEnumerable<ShapeTraceResult> CastShape( PhysicsShapeQueryParameters3D parameters )
+	{
+		var result = _space.IntersectShape( parameters );
+
+		foreach ( var r in result )
+		{
+
+			foreach ( var key in r.Keys )
+			{
+				GD.Print( $"{key}: {r[key]} ({r[key].GetType()})" );
+			}
+
+			yield return new ShapeTraceResult
+			{
+				ColliderId = (int)r["collider_id"],
+				Collider = r["collider"],
+				Shape = r["shape"],
+				Rid = r["rid"]
+			};
+		}
+
+	}
+
 }
