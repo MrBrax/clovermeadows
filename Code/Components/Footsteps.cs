9db7af08388512085adab79d0801f8e6abf854b7
CATERPILLAR\Braxen <braxen@braxnet.org>
2024-06-12 14:50:05 +0200

    footsteps component

---
+++ Code/Components/Footsteps.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace vcrossing.Code.Components;
+
+public partial class Footsteps : AudioStreamPlayer3D
+{
+
+    // [Export] public AudioStreamPlayer3D FootstepPlayer { get; set; }
+
+    [Export] public Godot.Collections.Array<AudioStream> FootstepSounds { get; set; }
+
+    public void PlayFootstep()
+    {
+        Stream = FootstepSounds.PickRandom();
+        Play();
+    }
+
+    /* public void PlayFootstepRight()
+	{
+		FootstepPlayer.Play();
+	} */
+
+}
