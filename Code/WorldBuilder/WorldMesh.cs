4434dfa7c97a6ca00c7a5223cf7ae9507a529e7b
Braxen <braxen@braxnet.org>
2024-06-21 00:58:25 +0200

    refactor: Hide world mesh based on player position

--- Code/WorldBuilder/WorldMesh.cs
+++ Code/WorldBuilder/WorldMesh.cs
@@ -7,5 +7,26 @@ public partial class WorldMesh : Node3D
 
 	[Export] public SurfaceData Surface;
 
+	[Export] public Node3D MeshHidePosition;
+
+	public override void _Process( double delta )
+	{
+		base._Process( delta );
+
+		if ( MeshHidePosition == null ) return;
+
+		var player = GetNodeOrNull<Player.PlayerController>( "/root/Main/Player" );
+		if ( player == null ) return;
+
+		if ( player.GlobalPosition.Z < MeshHidePosition.GlobalPosition.Z )
+		{
+			Visible = false;
+		}
+		else
+		{
+			Visible = true;
+		}
+	}
+
 
 }
