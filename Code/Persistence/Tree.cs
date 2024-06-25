d68a81181f8e23abef88d7484a7cb0b60438afb0
Braxen <braxen@braxnet.org>
2024-06-25 20:52:41 +0200

    tree type

---
+++ Code/Persistence/Tree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace vcrossing.Code.Persistence;
+
+public class Tree : PersistentItem
+{
+	[JsonInclude] public DateTime LastFruitDrop { get; set; } = DateTime.UnixEpoch;
+
+	public override void GetNodeData( Node3D node )
+	{
+		base.GetNodeData( node );
+		if ( node is Items.Tree tree )
+		{
+			LastFruitDrop = tree.LastFruitDrop;
+			// GrowProgress = plant.GrowProgress;
+		}
+	}
+
+	public override void SetNodeData( Node3D node )
+	{
+		base.SetNodeData( node );
+		if ( node is Items.Tree tree )
+		{
+			tree.LastFruitDrop = LastFruitDrop;
+			// plant.GrowProgress = GrowProgress;
+		}
+	}
+
+}
