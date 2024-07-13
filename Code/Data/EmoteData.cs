d2b52d7aeb7cfaab6745de845a65a4e31fcd1da8
Braxen <braxen@braxnet.org>
2024-07-13 22:29:07 +0200

    emote data

---
+++ Code/Data/EmoteData.cs
@@ -0,0 +1,17 @@
+namespace vcrossing.Code.Data;
+
+[GlobalClass]
+public partial class EmoteData : Resource
+{
+	[Export] public string Name { get; set; } = "Emote";
+	[Export] public Texture Texture { get; set; }
+	[Export] public float DisplayDuration { get; set; } = 2.0f;
+	[Export] public bool Loop { get; set; } = false;
+	[Export] public string Animation { get; set; } = "default";
+
+	[Export] public Tween.TransitionType TransitionAppearType { get; set; } = Tween.TransitionType.Linear;
+	[Export] public Tween.TransitionType TransitionDisappearType { get; set; } = Tween.TransitionType.Linear;
+
+	[Export] public float TransitionAppearTime { get; set; } = 0.5f;
+	[Export] public float TransitionDisappearTime { get; set; } = 0.5f;
+}
