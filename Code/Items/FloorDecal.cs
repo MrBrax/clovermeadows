4c45e0281dec0176aadd7a147f3fe70b20fc3f7e
Braxen <braxen@braxnet.org>
2024-07-20 23:32:11 +0200

    animated floor decal

--- Code/Items/FloorDecal.cs
+++ Code/Items/FloorDecal.cs
@@ -12,8 +12,16 @@ public partial class FloorDecal : WorldItem
 
 	private static Dictionary<string, StandardMaterial3D> _materials = new();
 
+	private float _animTimer = 0;
+	private float _animSpeed = 0.1f;
+	private bool _isAnimated;
+	private AtlasTexture _atlas;
+
 	public void UpdateDecal()
 	{
+		_atlas?.Dispose();
+		_atlas = null;
+
 		if ( !string.IsNullOrWhiteSpace( TexturePath ) )
 		{
 			// decal.TextureAlbedo = Loader.LoadResource<Texture2D>( TexturePath );
@@ -29,44 +37,101 @@ public partial class FloorDecal : WorldItem
 
 			var texture = ImageTexture.CreateFromImage( image );
 
-			// Decal.TextureAlbedo = texture;
+			_isAnimated = TexturePath.GetFile().StartsWith( "anim_" );
 
-			/* var material = new StandardMaterial3D
+			Logger.Info( "FloorDecal", $"TexturePath: {TexturePath}, isAnimated: {_isAnimated} ({TexturePath.GetFile()})" );
+
+			if ( !_isAnimated )
 			{
-				AlbedoTexture = texture,
-				TextureFilter = BaseMaterial3D.TextureFilterEnum.Nearest,
-				Transparency = BaseMaterial3D.TransparencyEnum.AlphaScissor
-			}; */
 
-			StandardMaterial3D material;
+				StandardMaterial3D material;
 
-			if ( _materials.ContainsKey( TexturePath ) )
-			{
-				Logger.Info( "FloorDecal", $"Reusing existing material for {TexturePath}: {_materials[TexturePath]}" );
-				material = _materials[TexturePath];
+				if ( _materials.ContainsKey( TexturePath ) )
+				{
+					Logger.Info( "FloorDecal", $"Reusing existing material for {TexturePath}: {_materials[TexturePath]}" );
+					material = _materials[TexturePath];
+				}
+				else
+				{
+					material = new StandardMaterial3D
+					{
+						AlbedoTexture = texture,
+						TextureFilter = BaseMaterial3D.TextureFilterEnum.Nearest,
+						Transparency = BaseMaterial3D.TransparencyEnum.AlphaScissor,
+					};
+
+					Logger.Info( "FloorDecal", $"Created new material for {TexturePath}: {material}" );
+					_materials.Add( TexturePath, material );
+				}
+
+				Mesh.Mesh = Mesh.Mesh.Duplicate() as QuadMesh; // Clone the mesh to avoid modifying the original material
+
+				(Mesh.Mesh as QuadMesh).Material = material;
+
+				Logger.Info( "FloorDecal", $"Updated decal texture to {TexturePath}" );
+
 			}
 			else
 			{
-				material = new StandardMaterial3D
+
+				ShaderMaterial shader = new ShaderMaterial();
+
+				shader.Shader = Loader.LoadResource<Shader>( "res://shaders/animated_floor_decal_new.tres" );
+
+				shader.SetShaderParameter( "texture", texture );
+
+				Mesh.Mesh = Mesh.Mesh.Duplicate() as QuadMesh; // Clone the mesh to avoid modifying the original material
+
+				(Mesh.Mesh as QuadMesh).Material = shader;
+
+				/* AtlasTexture atlas = new AtlasTexture();
+				atlas.Atlas = texture;
+				atlas.Region = new Rect2( 0, 0, 32, 32 );
+
+				_atlas = atlas;
+
+				Mesh.Mesh = Mesh.Mesh.Duplicate() as QuadMesh; // Clone the mesh to avoid modifying the original material
+
+				(Mesh.Mesh as QuadMesh).Material = new StandardMaterial3D
 				{
-					AlbedoTexture = texture,
+					AlbedoTexture = atlas,
 					TextureFilter = BaseMaterial3D.TextureFilterEnum.Nearest,
 					Transparency = BaseMaterial3D.TransparencyEnum.AlphaScissor,
 				};
 
-				Logger.Info( "FloorDecal", $"Created new material for {TexturePath}: {material}" );
-				_materials.Add( TexturePath, material );
+				Logger.Info( "FloorDecal", $"Updated decal texture to animated {TexturePath}, atlas: {atlas}, region: {atlas.Region}" ); */
+
 			}
 
-			Mesh.Mesh = Mesh.Mesh.Duplicate() as QuadMesh; // Clone the mesh to avoid modifying the original material
-
-			(Mesh.Mesh as QuadMesh).Material = material;
-
-			Logger.Info( "FloorDecal", $"Updated decal texture to {TexturePath}" );
 		}
 		else
 		{
 			Logger.Warn( "FloorDecal", "TexturePath is null" );
 		}
 	}
+
+	/* public override void _Process( double delta )
+	{
+
+		if ( _isAnimated )
+		{
+			_animTimer += (float)delta;
+
+			/* if ( _animTimer >= _animSpeed )
+			{
+				_animTimer = 0;
+
+				var region = _atlas.Region;
+				region.Position += new Vector2( 32, 0 );
+
+				if ( region.Position.X >= 32 * 4 )
+				{
+					region.Position = new Vector2( 0, 0 );
+				}
+
+				_atlas.Region = region;
+
+				GD.Print( $"_animTimer: {_animTimer}, _animSpeed: {_animSpeed}, region: {region}" );
+			} */
+
 }
