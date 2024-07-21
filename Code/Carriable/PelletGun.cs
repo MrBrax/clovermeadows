5e9e59e30c550220a5a8bb65267e9ace77602c62
Braxen <braxen@braxnet.org>
2024-07-21 16:47:15 +0200

    pellet gun stub

---
+++ Code/Carriable/PelletGun.cs
@@ -0,0 +1,14 @@
+using System;
+using vcrossing.Code.Data;
+using vcrossing.Code.Inventory;
+using vcrossing.Code.Items;
+using vcrossing.Code.Persistence;
+using vcrossing.Code.Player;
+using vcrossing.Code.WorldBuilder;
+
+namespace vcrossing.Code.Carriable;
+
+public sealed partial class PelletGun : BaseCarriable
+{
+
+}
